d9a17b738904d1c5860673a49427ef7e5e0be249
dmytro <dmytrygarden@hotmail.com>
2025-10-06 23:47:37 +0200

    small fixes

--- internal/app/app.go
+++ internal/app/app.go
@@ -6,17 +6,17 @@ import (
 	"github.com/fiskaly/coding-challenges/signing-service-challenge/api"
 	v0 "github.com/fiskaly/coding-challenges/signing-service-challenge/api/v0"
 	"github.com/fiskaly/coding-challenges/signing-service-challenge/internal/devices"
-	inmemory2 "github.com/fiskaly/coding-challenges/signing-service-challenge/internal/persistence/inmemory"
-	crypto2 "github.com/fiskaly/coding-challenges/signing-service-challenge/pkg/crypto"
+	"github.com/fiskaly/coding-challenges/signing-service-challenge/internal/persistence/inmemory"
+	"github.com/fiskaly/coding-challenges/signing-service-challenge/pkg/crypto"
 )
 
 // NewServer wires together application dependencies and returns a configured HTTP server.
 func NewServer(listenAddress string) *api.Server {
-	repository := inmemory2.NewDeviceRepository()
-	keyStore := inmemory2.NewKeyStore()
-	keyGenerator := crypto2.NewDefaultKeyGenerator()
-	signerFactory := crypto2.NewSignerFactory()
-	signatureStore := inmemory2.NewSignatureStore()
+	repository := inmemory.NewDeviceRepository()
+	keyStore := inmemory.NewKeyStore()
+	keyGenerator := crypto.NewDefaultKeyGenerator()
+	signerFactory := crypto.NewSignerFactory()
+	signatureStore := inmemory.NewSignatureStore()
 
 	coreService := devices.NewService(repository, keyStore, keyGenerator, signerFactory, signatureStore)
 	loggingService := devices.NewLoggingService(coreService, func(event string, fields map[string]interface{}) {
